42dd4b532367189920f89c33def154aac021cc48
BLWings <Luqmaanhakim1@outlook.com>
2026-02-19 14:02:08 +0000

    Update read_pdf.py

--- read_pdf.py
+++ read_pdf.py
@@ -15,9 +15,24 @@ location = text[4]
 course_type = text[6]
 cost = text[8]
 
+learning_objectives = []
+provided_materials = []
+
+divider = text.index("Provided Materials")
+
+
+
+for x in range(10, divider):
+    learning_objectives.append(text[x])
+
+for x in range(divider + 1, len(text)):
+    provided_materials.append(text[x])
+
 
 print(title)
 print(instructor)
 print(location)
 print(course_type)
 print(cost)
+print(learning_objectives)
+print(provided_materials)
