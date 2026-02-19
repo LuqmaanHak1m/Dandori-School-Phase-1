c5a161f0c99dd00df67823ffe3319b780e87b246
BLWings <Luqmaanhakim1@outlook.com>
2026-02-19 13:49:04 +0000

    Update read_pdf.py

--- read_pdf.py
+++ read_pdf.py
@@ -2,4 +2,11 @@ from pypdf import PdfReader
 
 reader = PdfReader("class_001_The_Art_of_Wondrous_Waffle_Weaving.pdf")
 page = reader.pages[0]
-print(page.extract_text().strip("•£"))
+
+text = page.extract_text().replace("•", "")
+
+text = text.strip()
+
+print(list(text))
+
+print(text)
