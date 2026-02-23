d6dc10bf75bc513439603a864292f12337eaf6b0
BLWings <Luqmaanhakim1@outlook.com>
2026-02-23 15:44:58 +0000

    LLM test

--- utils/rag.py
+++ utils/rag.py
@@ -5,6 +5,7 @@ from chromadb.config import Settings
 import os
 from openai import OpenAI
 import requests
+import re
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,7 +14,6 @@ endpoint=os.getenv("ENDPOINT")
 
 chat_client = OpenAI(api_key=api_key, 
                     base_url=endpoint)
-
 
 def load_data() -> list[dict]:
     data = pd.read_csv("all_courses.csv")
@@ -78,6 +78,37 @@ def load_collection():
     else:
         return collection
 
+
+def call_LLM(collection, query= "", top_results = 1, temp = 0.4, max_tokens=512):
+
+    relevant_info = collection.query(
+                            query_texts=[query],
+                            n_results=top_results 
+                            )
+
+    # Embed the collection before the query 
+    embedded_query = f"""{relevant_info}\n {query}"""
+    
+    stuff = rerank_results(query, relevant_info['documents'][0], relevant_info['metadatas'][0], relevant_info['ids'][0], relevant_info['distances'][0])
+    print(stuff)
+
+    # if query != "":
+    #     # Get a response from the client
+    #     response = chat_client.chat.completions.create(
+    #         model="google/gemini-2.0-flash-001",
+    #         messages=[
+    #             {"role": "system", "content": """
+    #              You are The Pandaroo, the friendly mascot of a whimsical, adult-only wellbeing school. Your role is to inform users about upcoming courses, classes and events. Communicate clearly, calmly and warmly. Add a light touch of whimsy when appropriate but prioritise accuracy and clarity. Do not give advice, coaching or wellbeing guidance beyond what is described in the course details. Present dates, times, locations and descriptions reliably. If information is missing or uncertain, say so plainly.You are welcoming and gentle but primarily informational.
+    #              """},
+    #             {"role": "user", "content": embedded_query}
+    #         ],
+    #         temperature=temp,
+    #         max_tokens=max_tokens
+    #     )
+    #     return response.choices[0].message.content
+    # else:
+        # return "No query was received"
+
     
 if __name__ == "__main__":
     print("Starting")
@@ -87,12 +118,18 @@ if __name__ == "__main__":
     print("Done")
     print(f"Collection: {collection}")
 
-    results = collection.query(
-        query_texts=["Pastry and Magic"],
-        n_results=5
-    )
+    # Test Collection
+    # results_num = 10
 
-    for i in range(5):
-        print(results["documents"][0][i])
+    # results = collection.query(
+    #     query_texts=["What courses are there for less than £75 in Norfolk?"],
+    #     n_results=results_num
+    # )
 
+    # for i in range(results_num):
+    #     print(results["documents"][0][i])
+
+    response = call_LLM(collection, "What courses are there for less than £75 in Norfolk?", 10)
+
+    # print(response)
 
